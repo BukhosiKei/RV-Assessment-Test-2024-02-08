eddc59ed6d6a73bd38440b4e02ed13464151d126
BukhosiKei <124839497+BukhosiKei@users.noreply.github.com>
2024-02-11 12:54:04 +0200

    Add files via upload

---
+++ Assessment-Test-2024-02-08.developer.tech/Product.cs
@@ -0,0 +1,14 @@
+// Product.cs
+
+using System;
+
+namespace RocvestAssessment.Models
+{
+    public class Products
+    {
+        public required string Title { get; set; }
+        public required string Image { get; set; }
+        public decimal Price { get; set; }
+ 
+    }
+}
